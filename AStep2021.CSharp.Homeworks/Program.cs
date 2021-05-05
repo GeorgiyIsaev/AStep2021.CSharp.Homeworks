ecc6913fdcd658fccffbec9992d7d3e8791471d3
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-05 20:30:36 +0300

    fixed task01(HW01) иное решение

--- AStep2021.CSharp.Homeworks/Program.cs
+++ AStep2021.CSharp.Homeworks/Program.cs
@@ -14,20 +14,8 @@ namespace AStep2021.CSharp.HW01
 
         }
         static void Problem01()
-        {
-            /*1.	Даны целые положительные числа A, B, C. Значение
-            этих чисел программа должна запрашивать у пользо-
-            вателя. На прямоугольнике размера A*B размещено
-            максимально возможное количество квадратов со
-            стороной C. Квадраты не накладываются друг на
-            друга. Найти количество квадратов, размещенных на
-            прямоугольнике, а также площадь незанятой части
-            прямоугольника.
-            Необходимо предусмотреть служебные сообщения
-            в случае, если в прямоугольнике нельзя разместить ни
-            одного квадрата со стороной С (например, если зна-
-            чение С превышает размер сторон прямоугольника).*/
-
+        { 
+           /*Задача 01*/
             int a, b, c;
             a = IntRead("A");
             b = IntRead("B");
@@ -37,23 +25,23 @@ namespace AStep2021.CSharp.HW01
             {
                 int sRect = b * a;
                 int sSquare = c * c;
-                int countSquare = sRect / sSquare;
-                Console.WriteLine("Колличество квадратов со стронами С(" + c + ") в прямоугольнике АB(" + a + "," + b + ") равно:"+ countSquare);
+                //    int countSquare = sRect / sSquare;
+                //    Console.WriteLine("Колличество квадратов со стронами С(" + c + ") в прямоугольнике АB(" + a + "," + b + ") равно:"+ countSquare);
+                //    int sSurplus = sRect - countSquare * sSquare;
+                //    Console.WriteLine("Площадь незанятой части прямоугольника: "+ sSurplus);
 
-                int sSurplus = sRect - countSquare * sSquare;
-                Console.WriteLine("Площадь незанятой части прямоугольника: "+ sSurplus);
-
-
-
-
+                int storonaA = a / c;
+                int storonaB = b / c;
+                int countSquareAB = storonaA * storonaB;
+                Console.WriteLine("Колличество квадратов со стронами С(" + c + ") в прямоугольнике АB(" + a + "," + b + ") равно:" + countSquareAB);
+                int sSurplus = sRect - countSquareAB * sSquare;
+                Console.WriteLine("Площадь незанятой части прямоугольника: " + sSurplus);
             }
             else
             {
                 Console.WriteLine("В прямоугольнике со стронами АB("+a+"," +b+"),");
                 Console.WriteLine("нельзя разместить квадрат со строной С("+c+")");
             }
-
-
         }
 
         static int IntRead(string nameval = "")
