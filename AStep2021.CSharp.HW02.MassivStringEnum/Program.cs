d73d1fdc2d3920793dd09aa35fe17f597821ad09
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-07 23:35:19 +0300

    Add Task01 (HW02)

--- AStep2021.CSharp.HW02.MassivStringEnum/Program.cs
+++ AStep2021.CSharp.HW02.MassivStringEnum/Program.cs
@@ -11,12 +11,40 @@ namespace AStep2021.CSharp.HW02.MassivStringEnum
         static void Main(string[] args)
         {
             Task01();
+            Console.ReadKey();
         }
 
         static void Task01()
         {
             /*1.	Сжать массив, удалив из него все 0 и, заполнить ос-
             вободившиеся справа элементы значениями –1*/
+            int[] massiv = new int[10];
+
+            Random rand = new Random();
+            for(int i=0; i< massiv.Length; i++)
+            {
+                massiv[i] = rand.Next(0, 3);
+                Console.Write(massiv[i] + "\t");
+            }
+
+            for (int i = 0; i < massiv.Length;)
+            {
+                if (massiv[i] == 0)
+                {
+                    for (int j = i; j < massiv.Length - 1; j++)
+                    {
+                        massiv[j] = massiv[j + 1];
+                        massiv[massiv.Length - 1] = -1;
+                    }
+                }
+                else { i++; }
+            }
+            Console.WriteLine();
+            foreach(int i in massiv)
+            {
+                Console.Write(i + "\t");
+            }
+
 
 
         }
