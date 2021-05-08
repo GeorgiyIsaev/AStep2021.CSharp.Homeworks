63e36d3ddd0d6b95030c10ffe131d4e326264987
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-08 14:22:37 +0300

    Create Project HW03

---
+++ AStep2021.CSharp.HW03.ClassAndStruct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW03.ClassAndStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
