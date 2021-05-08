4144e27846e4b2a5884bd43af08e854ab86d726a
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-08 18:49:03 +0300

    Add (Добавлены классы для других фигур)

---
+++ AStep2021.CSharp.HW04.Task01.Forms/Ellipse.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW04.Task01.Forms
+{
+    class Ellipse : Form
+    {
+        /*Треугольник, / * Triangle,
+       
+         * Квадрат, * Square,
+      
+         * Ромб,    * Rhombus,
+     
+         * Прямоугольник,    * Rectangle,
+     
+         * Параллелограмм,    * Parallelogram,
+     
+         * Трапеция,    * Trapezoid, 
+      
+         * Круг,   * Circle,
+   
+         * Эллипс     * Ellipse*/
+
+        int AB;
+        int BC;
+        int CA;
+        public Ellipse(int AB,int BC,int CA)
+        {
+            this.AB = AB;
+            this.BC= BC;
+            this.CA= CA;
+        }
+
+        public override double PForm()
+        {
+            double perimeter = AB + BC + CA;
+            return perimeter;           
+        }
+
+        public override double SForm()
+        {
+            //Формула Герона 
+            //S = √ p * (p − a) * (p − b) * (p − c)​,
+            //полупериметр p = (a + b + c) : 2
+            double p = PForm() / 2;
+            double square = Math.Sqrt(p * (p - AB) * p*(p - BC) * p*(p - CA));
+            return square;
+        }
+    }
+}
