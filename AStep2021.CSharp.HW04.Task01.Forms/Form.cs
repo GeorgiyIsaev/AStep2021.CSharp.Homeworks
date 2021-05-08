d5e502bea7fc4f80a763a3d205090ce80d7d747c
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-08 18:40:05 +0300

    add class  Triangle

---
+++ AStep2021.CSharp.HW04.Task01.Forms/Form.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW04.Task01.Forms
+{
+    abstract class Form
+    {
+        abstract public double SForm();
+        abstract public double PForm();
+    }
+}
