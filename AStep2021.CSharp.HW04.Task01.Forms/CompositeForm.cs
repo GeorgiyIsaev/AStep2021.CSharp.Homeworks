663b261a72affc307be96e3ff31394ad9f129ee1
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-08 19:40:29 +0300

    Add  class CompositeForm

---
+++ AStep2021.CSharp.HW04.Task01.Forms/CompositeForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW04.Task01.Forms
+{
+    class CompositeForm : Form
+    {
+        List<Form> compositeForm;
+        
+
+
+        public override double PForm()
+        {
+            return 1;
+        }
+
+        public override double SForm()
+        {
+            return 1;
+        }
+    }
+}
