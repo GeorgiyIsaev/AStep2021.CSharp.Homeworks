a0df863d5d1a0c0a51a8b7bfb98162ac75852251
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-08 14:50:29 +0300

    Create file StructTask01Task07.cs (Структуры и перечисления (Task 01-07) перенесены в отдельный файл)

--- AStep2021.CSharp.HW03.ClassAndStruct/Program.cs
+++ AStep2021.CSharp.HW03.ClassAndStruct/Program.cs
@@ -6,69 +6,8 @@ using System.Threading.Tasks;
 
 namespace AStep2021.CSharp.HW03.ClassAndStruct
 {
-    
-    
-    
-    public struct Article
-    {
-        public int idCod;
-        public string name;
-        public int price;
-        public ArticleType articleType;
-    }
-    public struct Client
-    {
-        public string FIO;
-        public string address;
-        public string telefon;
-        public int countOrder;
-        public int fullOrderPrice;
-        public ClientType clientType;
-    }
-    public struct RequestItem
-    {
-        public string name;
-        public int count;
-    }
-    public struct Request
-    {
-        public int idCod;
-        public Client client;
-        public string data;
-        public List<Article> articles;
-        public PayType payType;
-
-        public int fulPrice
-        {
-            get
-            {
-                int fullPriceit = 0;
-                foreach(Article article in articles)
-                {
-                    fullPriceit += article.price;
-                }
-                return fullPriceit;
-            }
-        }
-    }
-
-    public enum ArticleType
-    {
-        low,
-        medium,
-        premium
-    }
-    public enum ClientType
-    {
-        common,
-        vip
-    }
-    public enum PayType
-    {
-        cash, 
-        card, 
-        check
-    }
+      
+   
 
 
     class Program
---
+++ AStep2021.CSharp.HW03.ClassAndStruct/StructTask01Task07.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW03.ClassAndStruct
+{
+    public struct Article
+    {
+        public int idCod;
+        public string name;
+        public int price;
+        public ArticleType articleType;
+    }
+    public struct Client
+    {
+        public string FIO;
+        public string address;
+        public string telefon;
+        public int countOrder;
+        public int fullOrderPrice;
+        public ClientType clientType;
+    }
+    public struct RequestItem
+    {
+        public string name;
+        public int count;
+    }
+    public struct Request
+    {
+        public int idCod;
+        public Client client;
+        public string data;
+        public List<Article> articles;
+        public PayType payType;
+
+        public int fulPrice
+        {
+            get
+            {
+                int fullPriceit = 0;
+                foreach (Article article in articles)
+                {
+                    fullPriceit += article.price;
+                }
+                return fullPriceit;
+            }
+        }
+    }
+
+    public enum ArticleType
+    {
+        low,
+        medium,
+        premium
+    }
+    public enum ClientType
+    {
+        common,
+        vip
+    }
+    public enum PayType
+    {
+        cash,
+        card,
+        check
+    }
+}
