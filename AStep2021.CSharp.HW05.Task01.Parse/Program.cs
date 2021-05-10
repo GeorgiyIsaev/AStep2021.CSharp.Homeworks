76a79a3b620db350a1259730555b8c4587cf8eb6
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-10 17:00:16 +0300

    Add hw05 task01

--- AStep2021.CSharp.HW05.Task01.Parse/Program.cs
+++ AStep2021.CSharp.HW05.Task01.Parse/Program.cs
@@ -6,10 +6,52 @@ using System.Threading.Tasks;
 
 namespace AStep2021.CSharp.HW05.Task01.Parse
 {
+    public static class LinearEquation
+    {
+        static double A;
+        static double B;
+
+       
+        public static bool Parse(string valStr)
+        {
+            string[] separator = { " ", "," };
+            string[] lineItem = valStr.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            try{
+                A = Convert.ToInt32(lineItem[0]);
+                B = Convert.ToInt32(lineItem[1]);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        static public string Info()
+        {
+            string str = A + "*X + " + B + "*Y = 0";
+            return str;
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            ReadStr();
+            Console.WriteLine("Уравнения: "  + LinearEquation.Info());
+            
+
+        }
+        static void ReadStr()
+        {
+            Console.WriteLine("Введите два числа через пробел или через запяту:");
+            string tempStr = Console.ReadLine();
+            if (!LinearEquation.Parse(tempStr))
+            {
+                Console.WriteLine("ERROR: Не вернный ввод!");
+                ReadStr();
+            }
         }
     }
 }
