d5d2bd504c0e1c0cbfe165ac41fd8eb118bf95c3
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-10 18:19:00 +0300

    fixed HW05 Task03 - Добвлены перегурзки

--- AStep2021.CSharp.HW05.Task03.Complex/Program.cs
+++ AStep2021.CSharp.HW05.Task03.Complex/Program.cs
@@ -6,8 +6,66 @@ using System.Threading.Tasks;
 
 namespace AStep2021.CSharp.HW05.Task03.Complex
 {
-    
-    
+    class Complex
+    {
+        private double x;
+        private double y;
+
+        public Complex(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            string valStr = Math.Round(x, 2) + " i" + y;
+            return valStr;
+        }
+
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(
+                a.x * b.x - a.y * b.y, 
+                a.y * b.x + a.x * b.y
+                );
+        }
+        public static Complex operator /(Complex a, Complex b)
+        {
+            return new Complex(
+                (a.x * b.x + a.y * b.y) / (b.x * b.x + b.y * b.y), 
+                (a.y * b.x - a.x * b.y) / (b.x * b.x + b.y * b.y)
+                );
+        }
+
+        public static Complex operator *(double a, Complex b)
+        {
+            return new Complex(a * b.x, a * b.y);
+        }
+        public static Complex operator *(Complex b, double a)
+        {
+            return a * b;
+        }             
+
+        public static Complex operator -(Complex a, double b)
+        {
+            return new Complex(a.x - b, a.y - b);
+        }
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.x - b.x, a.y - b.y);
+        }
+
+        public static Complex operator +(Complex a, double b)
+        {
+            return new Complex(a.x + b, a.y + b);
+        }
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.x + b.x, a.y + b.y);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
