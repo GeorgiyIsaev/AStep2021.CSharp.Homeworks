2580f21717f1fd8a19f7ab3de51d61cbf56e73f6
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-10 16:25:18 +0300

    Create Project HW05.Task01

---
+++ AStep2021.CSharp.HW05.Task01.Parse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW05.Task01.Parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
