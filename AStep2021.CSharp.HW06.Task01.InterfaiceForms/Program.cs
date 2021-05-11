1586d1f135eaee729d2ffee7ca57d01eb4095ac0
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-11 19:19:19 +0300

    Create Project HW06.Task01

---
+++ AStep2021.CSharp.HW06.Task01.InterfaiceForms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW06.Task01.InterfaiceForms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
