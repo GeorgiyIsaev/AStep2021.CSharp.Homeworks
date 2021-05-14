285fba1e9700c0e26d4975870b3e21b5ed8243d3
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-14 20:09:33 +0300

    add  class Point3D : Point2D<int>

---
+++ AStep2021.CSharp.HW08.Task02.CollectionPoint/Point3D.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW08.Task02.CollectionPoint
+{
+    class Point3D : Point2D<int>
+    {
+        int z;
+
+        public int Z
+        {
+            get { return z; }
+            set { z = value; }
+        }
+
+        public Point3D(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(" x = {0};  y = {1};  z = {2}", X, Y, Z);
+        }
+    }
+}
