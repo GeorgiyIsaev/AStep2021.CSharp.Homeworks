06e31f45c759e8aa1feb0bb31625984cb1025797
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-14 19:26:24 +0300

    Создал пользовательское меню

--- AStep2021.CSharp.HW08.Task01.CollectionList/Program.cs
+++ AStep2021.CSharp.HW08.Task01.CollectionList/Program.cs
@@ -17,11 +17,50 @@ namespace AStep2021.CSharp.HW08.Task01.CollectionList
             dictionary.Add("США", "USA");
             dictionary.Add("Франция", "France");
             dictionary.Add("Китай", "China");
-            foreach (KeyValuePair<string, string> val in dictionary)
+           
+
+            Console.WriteLine("1 - Запросить перевод слова.");
+            Console.WriteLine("2 - Добавить слово.");
+            Console.WriteLine("3 - Показать весь словарь.");
+            Console.WriteLine("4 - ВЫХОД!.");
+ 
+            int param = IntRead("Выберите пункт меню");
+            switch (param)
             {
-                // keyValue.Value представляет класс Person
-                Console.WriteLine(val.Key + " - " + val.Value);
+                case 1: //Запросить перевод слова.
+
+                    break;
+
+                case 2: // Добавить слово
+                    break;
+
+                case 3: //Показать весь словарь
+                    foreach (KeyValuePair<string, string> val in dictionary)
+                    {                  
+                        Console.WriteLine(val.Key + " - " + val.Value);
+                    }
+                    break;
+
+                case 4: Environment.Exit(0); break;
             }
+
         }
+        static int IntRead(string nameval = "")
+        {
+            int val;
+            try
+            {
+                Console.Write(nameval + ": ");
+                val = Convert.ToInt32(Console.ReadLine());
+                if (val < 0) throw new Exception();
+            }
+            catch
+            {
+                Console.WriteLine("OШИБКА ВВОДА. Требуется ввести положительное число!");
+                val = IntRead(nameval);
+            }
+            return val;
+        }
+
     }
 }
