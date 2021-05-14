eb9b85d93e6e4a657482975fff3cdc9ecf232e40
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-14 19:16:50 +0300

    Add Dictionary

--- AStep2021.CSharp.HW08.Task01.CollectionList/Program.cs
+++ AStep2021.CSharp.HW08.Task01.CollectionList/Program.cs
@@ -10,6 +10,18 @@ namespace AStep2021.CSharp.HW08.Task01.CollectionList
     {
         static void Main(string[] args)
         {
+
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("Россия", "Russia");
+            dictionary.Add("Британия", "Great Britain");
+            dictionary.Add("США", "USA");
+            dictionary.Add("Франция", "France");
+            dictionary.Add("Китай", "China");
+            foreach (KeyValuePair<string, string> val in dictionary)
+            {
+                // keyValue.Value представляет класс Person
+                Console.WriteLine(val.Key + " - " + val.Value);
+            }
         }
     }
 }
