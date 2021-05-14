1baa02f6e7bc4a92063d5028fb3e91fad9ff9479
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-14 21:25:05 +0300

    Test in main

--- AStep2021.CSharp.HW08.Task04.CountDictionary/Program.cs
+++ AStep2021.CSharp.HW08.Task04.CountDictionary/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AStep2021.CSharp.HW08.Task04.CountDictionary
 {
@@ -10,6 +11,46 @@ namespace AStep2021.CSharp.HW08.Task04.CountDictionary
     {
         static void Main(string[] args)
         {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();     
+
+            string text = ReadTXT();
+            string[] separator = { "\n", "\r",".",",",":","-"};
+            string[] wordItems = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string wordItem in wordItems)
+            {                
+                if (!dictionary.ContainsKey(wordItem))
+                {
+                    dictionary.Add(wordItem, 1);
+                }
+                else
+                    dictionary[wordItem]++;
+            }
+            foreach (var val in dictionary)
+            {
+                Console.WriteLine(val.Key + " - " + val.Value);
+            }
+
+
         }
+        static string ReadTXT(string nameFile = "test.txt")
+        {
+            string text = "";
+            try
+            {
+                
+                using (var file = new StreamReader(nameFile, Encoding.Unicode))
+                {
+                    text = file.ReadToEnd();
+                }              
+            }
+            catch
+            {
+                Console.WriteLine("Файл не доступен!");
+            }
+            return text;
+        }
+
+
     }
 }
