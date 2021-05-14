054badb0cd1d5ba6b02604e609ff746af2805ab1
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-14 20:05:36 +0300

    add    class Point2D<T>

---
+++ AStep2021.CSharp.HW08.Task02.CollectionPoint/Point2D.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW08.Task02.CollectionPoint
+{
+    class Point2D<T> where T : struct, IComparable<T>
+    {
+        T x;
+        T y;
+
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public Point2D(T x, T y)
+        {
+            this.x = x; this.y = y;
+        }
+
+        public Point2D()
+        {
+            this.x = default(T); this.y = default(T);
+        }
+    }
+}
