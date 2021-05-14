8b129999501cbabd28cc77e370d0b11f1e141214
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-14 19:33:52 +0300

    add реализация всех меню

--- AStep2021.CSharp.HW08.Task01.CollectionList/Program.cs
+++ AStep2021.CSharp.HW08.Task01.CollectionList/Program.cs
@@ -17,31 +17,46 @@ namespace AStep2021.CSharp.HW08.Task01.CollectionList
             dictionary.Add("США", "USA");
             dictionary.Add("Франция", "France");
             dictionary.Add("Китай", "China");
-           
 
-            Console.WriteLine("1 - Запросить перевод слова.");
-            Console.WriteLine("2 - Добавить слово.");
-            Console.WriteLine("3 - Показать весь словарь.");
-            Console.WriteLine("4 - ВЫХОД!.");
- 
-            int param = IntRead("Выберите пункт меню");
-            switch (param)
+            while (true)
             {
-                case 1: //Запросить перевод слова.
+                Console.WriteLine("1 - Запросить перевод слова.");
+                Console.WriteLine("2 - Добавить слово.");
+                Console.WriteLine("3 - Показать весь словарь.");
+                Console.WriteLine("4 - ВЫХОД!.");
 
-                    break;
+                int param = IntRead("Выберите пункт меню");
+                switch (param)
+                {
+                    case 1: //Запросить перевод слова.
+                        Console.Write("Введите слово для перевода: ");
+                        string text = Console.ReadLine();
+                        foreach (KeyValuePair<string, string> val in dictionary)
+                        {
+                            if(text == val.Key)                           
+                                Console.WriteLine(val.Key + " - " + val.Value);
+                            if (text == val.Value)
+                                Console.WriteLine(val.Key + " - " + val.Value);
+                        }
+                        break;
 
-                case 2: // Добавить слово
-                    break;
+                    case 2: // Добавить слово
+                        Console.Write("Введите русское слово: ");
+                        string rus = Console.ReadLine();
+                        Console.Write("Введите англиское слово: ");
+                        string eng = Console.ReadLine();
+                        dictionary.Add(rus, eng);
+                        break;
 
-                case 3: //Показать весь словарь
-                    foreach (KeyValuePair<string, string> val in dictionary)
-                    {                  
-                        Console.WriteLine(val.Key + " - " + val.Value);
-                    }
-                    break;
+                    case 3: //Показать весь словарь
+                        foreach (KeyValuePair<string, string> val in dictionary)
+                        {
+                            Console.WriteLine(val.Key + " - " + val.Value);
+                        }
+                        break;
 
-                case 4: Environment.Exit(0); break;
+                    case 4: Environment.Exit(0); break;
+                }
             }
 
         }
@@ -53,6 +68,7 @@ namespace AStep2021.CSharp.HW08.Task01.CollectionList
                 Console.Write(nameval + ": ");
                 val = Convert.ToInt32(Console.ReadLine());
                 if (val < 0) throw new Exception();
+                Console.WriteLine("");
             }
             catch
             {
