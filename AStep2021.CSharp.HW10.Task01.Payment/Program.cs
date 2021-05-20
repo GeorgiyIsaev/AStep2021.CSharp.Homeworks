79ae3f808e5b7b48670f8bceaa69be308b577ea1
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-20 18:02:38 +0300

    Create project HW10

---
+++ AStep2021.CSharp.HW10.Task01.Payment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW10.Task01.Payment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
