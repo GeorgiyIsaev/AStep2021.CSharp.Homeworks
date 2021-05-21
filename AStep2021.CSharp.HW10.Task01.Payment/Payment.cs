24b6f56a5d4c11d4b4ad411266515ad58ede7601
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-21 23:06:23 +0300

    add реализация свойств

--- AStep2021.CSharp.HW10.Task01.Payment/Payment.cs
+++ AStep2021.CSharp.HW10.Task01.Payment/Payment.cs
@@ -8,22 +8,18 @@ namespace AStep2021.CSharp.HW10.Task01.Payment
 {        
     class Payment
     {
-        static bool ifFormater
-        {
-            get
-            {
-                return true;
-            }
-        }
+        static bool ifFormaterSerialize => false;
 
         int countPaymentForDay; //оплата за день;
         int countDay; //количество дней;
         int fineForDay; //штраф за один день задержки оплаты;
         int countDayNotPayment; // количество дней задержи оплаты;
-       
-        int SumPaymentNotFine; // сумма к оплате без штрафа (вычисляемое поле);
-        int SumFine; // штраф (вычисляемое поле);
-        int SumPayment; // общая сумма к оплате (вычисляемое поле).  
+
+        int SumPaymentNotFine => countPaymentForDay * countDay; // сумма к оплате без штрафа (вычисляемое поле);
+        int SumFine => fineForDay * countDayNotPayment; // штраф (вычисляемое поле);
+        int SumPayment => SumFine + SumPaymentNotFine; // общая сумма к оплате (вычисляемое поле) 
+
+      
 
 
     }
