b92b72f8a0a8707c7462ea9cd6b64578e4c46f5f
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-25 23:04:38 +0300

    create new project HW11

---
+++ AStep2021.CSharp.HW11.Task01.ProductStore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW11.Task01.ProductStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
