a8dc84b0c8dc60e280ac54a82317616a41e7f951
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-27 01:14:45 +0300

    CREATE new prodject HW12

---
+++ AStep2021.CSharp.HW12.Task01.MonitorDiagonalSize/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStep2021.CSharp.HW12.Task01.MonitorDiagonalSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
