be3b43a4be65f2ca7437fa9352c2a57ab04ecdc1
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-26 23:19:25 +0300

    Магазин создан

--- AStep2021.CSharp.HW11.Task01.ProductStore/Program.cs
+++ AStep2021.CSharp.HW11.Task01.ProductStore/Program.cs
@@ -12,14 +12,30 @@ namespace AStep2021.CSharp.HW11.Task01.ProductStore
         {
             List<Store> stores = new List<Store>();
             string[] food = new[] { "Яблоко", "Картошка", "Капуста", "Хлеб", "Колбаски" };
-            Random rd = new Random();
-            foreach (string txt in food)
+            AddStore(food, stores, TypeProduct.Food);
+            string[] alcohol = new[] { "Пиво", "Вино", "Водка"};
+            AddStore(alcohol, stores, TypeProduct.Alcohol);
+            string[] hygiene = new[] { "Мыло", "Шампунь", "Зубная паста", "Губка" };
+            AddStore(hygiene, stores, TypeProduct.Hygiene);
+            string[] jewelry = new[] { "Картина", "Открытка", "Ленточка" };
+            AddStore(jewelry, stores, TypeProduct.Jewelry);
+            string[] medicines = new[] { "Асперин", "Анальгин", "Витаминка", "Аскарбинка"};
+            AddStore(medicines, stores, TypeProduct.Medicines);
+
+            foreach (Store store in stores)
             {
-                stores.Add(new Store(txt, rd.Next(10,70), TypeProduct.Food));
+                Console.WriteLine(store);
             }
+            Console.ReadKey();
 
-
-
+        }
+        static void AddStore(string[] masNameProduct, List<Store> stores, TypeProduct typeProduct)
+        {
+            Random rd = new Random();
+            foreach (string txt in masNameProduct)
+            {
+                stores.Add(new Store(txt, rd.Next(10, 70), typeProduct));
+            }
         }
 
     }
