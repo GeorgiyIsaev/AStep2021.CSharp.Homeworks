95600cbea7ebac82ff6a3c4fd2b78701e7ee460d
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-27 01:26:44 +0300

    Ширина и высота в пикселях

--- AStep2021.CSharp.HW12.Task01.MonitorDiagonalSize/Program.cs
+++ AStep2021.CSharp.HW12.Task01.MonitorDiagonalSize/Program.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Windows.Forms;
+using System.Drawing; //подключил в ссылках
+using System.Windows.Forms; //подключил в ссылках
 
 namespace AStep2021.CSharp.HW12.Task01.MonitorDiagonalSize
 {
     class Program
     {
         static void Main(string[] args)
-        {
-
+        {           
+            /*Размеры экрана в пикселях */
+            Console.Write($"Ширина экрана: {SystemInformation.PrimaryMonitorSize.Width}");
+            Console.WriteLine($" Высота: {SystemInformation.PrimaryMonitorSize.Height}"); ;
         }
     }
 }
