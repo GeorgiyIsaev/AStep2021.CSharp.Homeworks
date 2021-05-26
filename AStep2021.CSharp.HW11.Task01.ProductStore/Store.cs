6c731d755290e83c8288798cf4b810a902d448d4
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-26 23:03:46 +0300

    add свщйства класса

--- AStep2021.CSharp.HW11.Task01.ProductStore/Store.cs
+++ AStep2021.CSharp.HW11.Task01.ProductStore/Store.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace AStep2021.CSharp.HW11.Task01.ProductStore
 {
+    enum TypeProduct
+    {
+        Food,
+        Hygiene, //Гигиена
+        Jewelry, //Украшения
+        Alcohol,
+        Medicines
+    }
+    
+    
     class Store
     {
+        public Store(string name, int price, TypeProduct typeProduct)
+        {
+            Name = name;
+            Price = price;
+            TypeProduct = typeProduct;
+        }
+
+        public string Name { get; set; }
+        public int Price { get; set; }
+        internal TypeProduct TypeProduct { get; set; }
+
+
     }
 }
