104f5af4b03a0340ad3291dbf56ea3299c2ecb37
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-28 23:48:45 +0300

    Тест выбрасывания исключения при отризательно значении сторон

--- AStep2021.CSharp.HW06.Task01.InterfaiceForms/Program.cs
+++ AStep2021.CSharp.HW06.Task01.InterfaiceForms/Program.cs
@@ -11,22 +11,30 @@ namespace AStep2021.CSharp.HW06.Task01.Forms
         static void Main(string[] args)
         {
             List<Form> forms = new List<Form>();
-            forms.Add(new Circle(10));
-            forms.Add(new Ellipse(10, 10));
-            forms.Add(new Parallelogram(10, 10, 2));
-            forms.Add(new Rectangle(10, 10));
-            forms.Add(new Rectangle(10, 10));
-            forms.Add(new Square(10));
-            forms.Add(new Trapezoid(10, 10, 10, 10));
-            forms.Add(new Triangle(10, 10, 10));
-            CompositeForm compForm = new CompositeForm();
-            compForm.RandomContent();
-            forms.Add(compForm);
+            try
+            {
+                forms.Add(new Circle(10));
+                forms.Add(new Ellipse(10, 10));
+                forms.Add(new Parallelogram(10, 10, 2));
+                forms.Add(new Rectangle(10, 10));
+                forms.Add(new Rectangle(10, 10));
+                forms.Add(new Square(10));
+                forms.Add(new Trapezoid(10, 10, 10, 10));
+                forms.Add(new Triangle(10, -10, 10));
+                CompositeForm compForm = new CompositeForm();
+                compForm.RandomContent();
+                forms.Add(compForm);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
             foreach (Form form in forms)
             {
                 form.Print();
             }
+
 
 
             Console.ReadKey();
